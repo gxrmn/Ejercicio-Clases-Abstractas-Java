4bab480926a70c193434f0bac2c4fddcf6ba9aea
ing.german <germanxito03@gmail.com>
2026-05-30 18:31:58 -0600

    Agregando archivo html para visualizar codigo e impresion dinamica de la informacion

--- src/com/german/claseAbstractaPoo/form/EjemploForm.java
+++ src/com/german/claseAbstractaPoo/form/EjemploForm.java
@@ -1,9 +1,14 @@
 package com.german.claseAbstractaPoo.form;
 
+import com.german.claseAbstractaPoo.form.elementos.ElementoForm;
 import com.german.claseAbstractaPoo.form.elementos.InputForm;
 import com.german.claseAbstractaPoo.form.elementos.SelectForm;
 import com.german.claseAbstractaPoo.form.elementos.TextAreaForm;
 import com.german.claseAbstractaPoo.form.elementos.select.Opcion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class EjemploForm {
     public static void main(String[] args) {
@@ -18,16 +23,23 @@ public class EjemploForm {
         SelectForm lenguaje = new SelectForm("lenguaje");
         Opcion java = new Opcion("1", "Java");
         lenguaje.addOpcion(java);
-        lenguaje.addOpcion(new Opcion("2", "Python"));
-        lenguaje.addOpcion(new Opcion("3", "JavaScript"));
-        lenguaje.addOpcion(new Opcion("4", "TypeScript"));
-        lenguaje.addOpcion(new Opcion("5", "PHP"));
+        lenguaje.addOpcion(new Opcion("2", "Python"))
+        .addOpcion(new Opcion("3", "JavaScript"))
+        .addOpcion(new Opcion("4", "TypeScript"))
+        .addOpcion(new Opcion("5", "PHP"));
 
         username.setValor("jhone.doe");
         password.setValor("12345");
         email.setValor("jhoncoffe@email.com");
         edad.setValor("22");
+        experiencia.setValor("... 1 año de experiencia en desarrollo backend con java ...");
         java.setSelected(true);
 
+        List<ElementoForm> elementos = Arrays.asList(username,password,email,edad,experiencia,lenguaje);
+
+        for(ElementoForm e: elementos){
+            System.out.println(e.dibujarHtml());
+            System.out.println("<br>");
+        }
     }
 }
